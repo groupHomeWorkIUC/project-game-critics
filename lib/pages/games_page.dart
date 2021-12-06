0826e9f376dc0f2e38458fb1fee921883e973935
Ibrahim <61706383+ibrahim-yavuz@users.noreply.github.com>
2021-12-06 09:42:52 +0300

    redesigned games page

--- lib/pages/games_page.dart
+++ lib/pages/games_page.dart
@@ -16,10 +16,13 @@ class GamesPage extends GetView<GamesController> {
             padding: const EdgeInsets.only(left: 10, right: 10, bottom: 100),
             crossAxisSpacing: 10,
             mainAxisSpacing: 10,
-            crossAxisCount: controller.games.length < 2 ? 1 : 2,
-            childAspectRatio: 2 / 3,
+            crossAxisCount: 1,
+            childAspectRatio: 1.3,
             children: List.generate(controller.games.length, (index) {
-              return GamesContainer(game: controller.games.elementAt(index));
+              return GamesContainer(
+                game: controller.games.elementAt(index),
+                isAlone: controller.games.length < 2,
+              );
             }));
       }),
     );
@@ -28,12 +31,6 @@ class GamesPage extends GetView<GamesController> {
   buildAppBar() {
     return AppBar(
       elevation: 0,
-      leading: IconButton(
-          splashRadius: 20,
-          onPressed: () {
-            Get.back();
-          },
-          icon: const Icon(Icons.arrow_back)),
       actions: [
         IconButton(
           onPressed: () {},
--- lib/widgets/games_container.dart
+++ lib/widgets/games_container.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:project_game_critics/constants/constants.dart';
 import 'package:project_game_critics/helpers/themes/colors.dart';
 import 'package:project_game_critics/models/game.dart';
 import 'package:project_game_critics/widgets/review_container.dart';
 
 class GamesContainer extends StatefulWidget {
   final Game? game;
-  const GamesContainer({Key? key, this.game}) : super(key: key);
+  final bool? isAlone;
+  const GamesContainer({Key? key, this.game, this.isAlone}) : super(key: key);
 
   @override
   _GamesContainerState createState() => _GamesContainerState();
@@ -24,20 +26,29 @@ class _GamesContainerState extends State<GamesContainer> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Expanded(
-            flex: 4,
+            flex: 5,
             child: Image.network(
-              widget.game!.images!.first.link ??
-                  'https://upload.wikimedia.org/wikipedia/commons/6/6a/A_blank_flag.png',
-              fit: BoxFit.fitHeight,
+              widget.game!.images!.first.link ?? Constants.blankImage,
+              fit: BoxFit.cover,
               width: MediaQuery.of(context).size.width,
             ),
           ),
           const SizedBox(height: 5),
           Expanded(
             flex: 1,
-            child: Text(
-              widget.game!.name!,
-              style: Theme.of(context).textTheme.subtitle1,
+            child: Row(
+              children: [
+                Text(
+                  widget.game!.name!,
+                  style: Theme.of(context).textTheme.subtitle1,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(width: 10),
+                const ReviewContainer(
+                  review: 91,
+                )
+              ],
             ),
           ),
           Expanded(
@@ -49,65 +60,8 @@ class _GamesContainerState extends State<GamesContainer> {
               style: Theme.of(context).textTheme.subtitle2,
             ),
           ),
-          const SizedBox(height: 5),
-          Expanded(
-              child: Row(
-            children: const [
-              Spacer(),
-              ReviewContainer(
-                review: 91,
-              )
-            ],
-          ))
         ],
       ),
     );
   }
 }
-
-
-/*
-import 'package:flutter/material.dart';
-import 'package:project_game_critics/helpers/themes/colors.dart';
-import 'package:project_game_critics/models/game.dart';
-
-class GamesContainer extends StatefulWidget {
-  final Game? game;
-  const GamesContainer({Key? key, this.game}) : super(key: key);
-
-  @override
-  _GamesContainerState createState() => _GamesContainerState();
-}
-
-class _GamesContainerState extends State<GamesContainer> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: DarkThemeColors.secondaryBackgroundColor,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.network(
-            widget.game!.images!.first.link!,
-            fit: BoxFit.cover,
-          ),
-          const SizedBox(height: 10),
-          Text(
-            widget.game!.name!,
-            style: Theme.of(context).textTheme.bodyText1,
-          ),
-          const SizedBox(height: 10),
-          Text(
-            widget.game!.content!,
-            style: Theme.of(context).textTheme.subtitle2,
-          )
-        ],
-      ),
-    );
-  }
-}
-*/ 
